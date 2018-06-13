5fe09cffd02abc4294c868272467d69d0fb9655a
Ajeet Gill <ajeetshergill@gmail.com>
2018-06-12 21:15:00 -0700

    WPA working, still missing Https cert

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const PORT = process.env.PORT || 5000
 var app = express();
 
 // app.use(express.static(path.join(__dirname, './')))
-express()
+app
   .use(express.static(path.join(__dirname, 'demo1')))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
+// https.createServer(options, app).listen(PORT, () => console.log(`Listening on ${ PORT }`));
