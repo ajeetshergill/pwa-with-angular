0c4297d2ea29b065e396f065704b2b05614e1dff
Ajeet Gill <ajeetshergill@gmail.com>
2018-06-12 20:13:37 -0700

    started working on view engine

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const https = require('https')
+const PORT = process.env.PORT || 5000
+var app = express();
+
+// app.use(express.static(path.join(__dirname, './')))
+express()
+  .use(express.static(path.join(__dirname, 'demo1')))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
